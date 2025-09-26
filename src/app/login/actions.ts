bd5e7c5bb21315010bbc7d9e21062f9852897481
Pamela Ribeiro <contaadm154@gmail.com>
2025-09-26 20:33:36 +0000

    coloque a senha admin

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -7,7 +7,7 @@ import { AUTH_COOKIE_NAME } from '@/lib/constants';
 export async function login(prevState: any, formData: FormData) {
   const password = formData.get('password');
 
-  if (password === process.env.ADMIN_PASSWORD) {
+  if (password === 'admin') {
     cookies().set(AUTH_COOKIE_NAME, 'true', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
