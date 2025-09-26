197bd5154e3a21847dbf7d8239b0b06ae60696d0
Pamela Ribeiro <contaadm154@gmail.com>
2025-09-26 22:36:13 +0000

    centralize a logo  (_for element h1_)

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,10 +1,8 @@
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-14 items-center">
-            <div className="flex-1">
-                <h1 className="text-2xl font-bold font-headline">Pixbin</h1>
-            </div>
+        <div className="container flex h-14 items-center justify-center">
+            <h1 className="text-2xl font-bold font-headline">Pixbin</h1>
         </div>
     </header>
   );
