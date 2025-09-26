17995e2151cf825037116cc26bef3aa298512a47
Pamela Ribeiro <contaadm154@gmail.com>
2025-09-26 20:36:57 +0000

    coloca a senha admin e nao acontece nada

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -6,15 +6,16 @@ import { AUTH_COOKIE_NAME } from '@/lib/constants';
 
 export async function login(prevState: any, formData: FormData) {
   const password = formData.get('password');
+  const adminPassword = process.env.ADMIN_PASSWORD || 'admin';
 
-  if (password === 'admin') {
+  if (password === adminPassword) {
     cookies().set(AUTH_COOKIE_NAME, 'true', {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 60 * 60 * 24 * 7, // 1 week
       path: '/',
     });
-    redirect('/');
+    return redirect('/');
   }
 
   return {
